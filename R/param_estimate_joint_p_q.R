9637bd92d5a0cb4ca7e97facc442543729ec72f8
Zhen-Miao <zhenmiao@sas.upenn.edu>
2023-04-28 16:54:04 -0400

    updated parameter estimation method

--- R/param_estimate_joint_p_q.R
+++ R/param_estimate_joint_p_q.R
@@ -21,96 +21,27 @@
 #'
 #' @examples
 get_r_by_ct_mat_pq <- function(cell_type_set,
-                               r_by_c,
-                               cell_type_labels,
-                               n_features_per_cell,
-                               p_acc = 0.0005,
-                               q_acc = 0.0005,
-                               n_max_iter = 500) {
-
-  ## require r_by_c to have column names
-  if (is.null(colnames(r_by_c))) {
-    stop("the peak by cell matrix has to have column names")
-  }
-  ## save arguments
-  ## note, with this method, there cannot be any unused argument,
-  ## or error will happen
-  args_s <- list()
-  args_s <- append(args_s, mget(names(formals()), sys.frame(sys.nframe())))
-
-  ## get output with internal functions
-  outs <- do.call(.get_r_by_ct_mat_pq_int, args = args_s)
-  itermat_p_by_type <- outs$itermat_p_by_type
-  itermat_q_by_type <- outs$itermat_q_by_type
-  print(str(itermat_p_by_type))
-
-  ## remove total na values
-  for (gg in cell_type_set) {
-    mat_to_shrink <- itermat_p_by_type[[gg]]
-    mat_to_shrink <- mat_to_shrink[, !is.na(colSums(mat_to_shrink))]
-    itermat_p_by_type[[gg]] <- mat_to_shrink
-
-    mat_to_shrink2 <- itermat_q_by_type[[gg]]
-    mat_to_shrink2 <- mat_to_shrink2[!is.na(rowSums(mat_to_shrink2)), ]
-    itermat_q_by_type[[gg]] <- mat_to_shrink2
-  }
-
-  ## create new matrix to store the p
-  p_by_t_new <- matrix(nrow = n_features_per_cell, ncol = length(cell_type_set))
-  colnames(p_by_t_new) <- cell_type_set
-  for (gg in cell_type_set) {
-    p_by_t_new_w0 <- itermat_p_by_type[[gg]][, dim(itermat_p_by_type[[gg]])[2]]
-    p_by_t_new_w0[p_by_t_new_w0 < 0.0005] <- 0.0005
-    p_by_t_new[, gg] <- p_by_t_new_w0
-  }
-
-  # save q information
-  q_vec_new <- vector(length = 0L)
-  for (gg in cell_type_set) {
-    q_vec_op <- itermat_q_by_type[[gg]][dim(itermat_q_by_type[[gg]])[1], ]
-    clabel <- colnames(r_by_c)[cell_type_labels == gg]
-    names(q_vec_op) <- clabel
-    # print(sum(is.na(q_vec_op)))
-    q_vec_new <- append(q_vec_new, q_vec_op)
-  }
-  q_vec_new <- q_vec_new[colnames(r_by_c)]
-  return(list(p_by_t = p_by_t_new, q_vec = q_vec_new))
-}
-
-#' Internal function -- get the region by cell type matrix and capturing rate
-#'
-#'
-#' @importFrom methods is
-#' @param cell_type_set A vector containing all cell types
-#' @param r_by_c Input matrix, region (peak) by cell
-#' @param cell_type_labels A vector containing cell type labels
-#' @param n_features_per_cell The number of features in the matrix, can be calculated by dim(r_by_c)[1]
-#' @param p_acc The accuracy of p, default specified as 0.0005
-#' @param q_acc The accuracy of q, default specified as 0.0005
-#' @param n_max_iter The maximum iteration, default = 500
-#'
-#' @return A list of two items \itemize{
-#'   \item itermat_p_by_type recorded region by cell type matrix in each iteration
-#'   \item itermat_q_by_type recorded capturing rates in each iteration
-#' }
-#'  and
-#' @export
-#'
-#' @examples
-#' @keywords internal
-#' @noRd
-.get_r_by_ct_mat_pq_int <- function(cell_type_set,
                                     r_by_c,
                                     cell_type_labels,
                                     n_features_per_cell,
                                     p_acc = 0.0005,
                                     q_acc = 0.0005,
-                                    n_max_iter = 500) {
+                                    n_max_iter = 800,
+                                    verbose = T) {
+  ## require cell names provided
+  if (is.null(colnames(r_by_c))) {
+    stop("the peak by cell matrix has to have column names")
+  }
+
   ## save data to matrix
-  itermat_q_by_type <- rep(list(), length = length(cell_type_set))
-  names(itermat_q_by_type) <- cell_type_set
-  itermat_p_by_type <- rep(list(), length = length(cell_type_set))
-  names(itermat_p_by_type) <- cell_type_set
+  itermat_q_by_type <- vector(length = dim(r_by_c)[2])
+  names(itermat_q_by_type) <- colnames(r_by_c)
+  itermat_p_by_type <- matrix(nrow = n_features_per_cell, ncol = length(cell_type_set))
+  colnames(itermat_p_by_type) <- cell_type_set
+
+  p_by_t_new <- matrix(nrow = n_features_per_cell, ncol = length(cell_type_set))
+  colnames(p_by_t_new) <- cell_type_set
+
 
   ## make the matrix binary
   if (!is(r_by_c, "sparseMatrix")) {
@@ -123,6 +54,7 @@ get_r_by_ct_mat_pq <- function(cell_type_set,
     r_by_c_sub <- r_by_c[, cell_type_labels == gg]
     n_cell_sub <- dim(r_by_c_sub)[2]
     r_by_c_sub <- as.matrix(r_by_c_sub)
+    cell_names_sub <- colnames(r_by_c_sub)
     n_reads_in_cell <- colSums(r_by_c_sub)
     n_reads_in_region <- rowSums(r_by_c_sub)
 
@@ -157,11 +89,23 @@ get_r_by_ct_mat_pq <- function(cell_type_set,
       diff1 <- sum(abs(itermat_p[, numiters] - itermat_p[, numiters - 1]) / abs(itermat_p[, numiters]), na.rm = T) / n_features_per_cell
       diff2 <- sum(abs(itermat_q[numiters, ] - itermat_q[numiters - 1, ]) / abs(itermat_q[numiters, ]), na.rm = T) / n_cell_sub
     }
-    itermat_p_by_type[[gg]] <- itermat_p
-    itermat_q_by_type[[gg]] <- itermat_q
-    print(paste(gg, " completed"))
-    print(paste("diff1 = ", diff1))
-    print(paste("diff2 = ", diff2))
+
+    ## remove columns that contain na values
+    mat_to_shrink <- itermat_p
+    mat_to_shrink <- mat_to_shrink[, !is.na(colSums(mat_to_shrink))]
+    itermat_p_by_type[, gg] <- mat_to_shrink[, dim(mat_to_shrink)[2]]
+
+    mat_to_shrink2 <- itermat_q
+    mat_to_shrink2 <- mat_to_shrink2[!is.na(rowSums(mat_to_shrink2)), ]
+    itermat_q_by_type[cell_names_sub] <- mat_to_shrink2[dim(mat_to_shrink2)[1], ]
+
+    ## print progress
+    if(verbose){
+      print(paste(gg, " completed"))
+      print(paste("diff1 = ", diff1))
+      print(paste("diff2 = ", diff2))
+    }
+
   }
-  return(list(itermat_p_by_type = itermat_p_by_type, itermat_q_by_type = itermat_q_by_type))
+  return(list(p_by_t = itermat_p_by_type, q_vec = itermat_q_by_type))
 }
